8fbf8d0fb54075a982bd45fdcbf204436add3ec4
Patrick Whelan <patrickawhelan@gmail.com>
2020-02-11 14:54:01 -0500

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import GroceryList from "./GroceryList"
+
+const App = props => {
+  const groceryData = [
+    { id: 1, name: "Oranges" },
+    { id: 2, name: "Bananas" },
+    { id: 3, name: "Bread" }
+  ]
+
+  const groceryStoreSock =[
+    {id:1, name: "Oranges"},
+    {id:2, name: "Bananas"},
+    {id:4, name: "Kambucha"},
+    {id:5, name: "High Fructose Corn Syrup Crunch"}
+
+  ]
+
+  return (
+    <div>
+      <h1>Grocery List</h1>
+      
+    </div>
+  )
+}
+
+export default App
